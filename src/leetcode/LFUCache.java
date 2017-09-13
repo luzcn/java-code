c67f44903bac92c98fe96a4654743617384a16fb
Zheng Lu <zheng.lu@oracle.com>
2017-09-13 16:46:45 -0700

    9/13 update

--- src/leetcode/LFUCache.java
+++ src/leetcode/LFUCache.java
@@ -6,6 +6,7 @@ package leetcode;
 
 //  get(key) - Get the value of the key if the key exists in the cache, otherwise return -1.
 //  put(key, value) - Set or insert the value if the key is not already present.
+//
 // When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item.
 //
 // For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency),
@@ -22,51 +23,15 @@ public class LFUCache {
 
     private int capacity = 0;
 
-    private HashMap<Integer, CacheData> map = new HashMap<Integer, CacheData>();
-    private PriorityQueue<CacheData> minHeap = new PriorityQueue<CacheData>(Comparator.comparingInt(v -> v.frequency));
-
-
-    public LFUCache(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int get(int key) {
-
-        int result = -1;
-
-        if (map.containsKey(key)) {
-
-            CacheData toUpdate = map.get(key);
-            result = toUpdate.value;
-
-            // update the frequency
-            minHeap.remove(toUpdate);
-            map.put(key, new CacheData(key, result, toUpdate.frequency + 1));
-            minHeap.add(map.get(key));
-
-        }
-        return result;
-    }
-
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            CacheData toUpdate = map.get(key);
 
-            // update the frequency
-            minHeap.remove(toUpdate);
-            map.put(key, new CacheData(key, value, toUpdate.frequency + 1));
-            minHeap.add(map.get(key));
-        }
-        else {
-            if (map.size() == this.capacity) {
-                CacheData toRemove = minHeap.poll();
-                if (toRemove != null)
-                    map.remove(toRemove.key);
-            }
-            map.put(key, new CacheData(key, value, 1));
-            minHeap.add(map.get(key));
-        }
     }
+
+    public int get(int key){
+        return 0;
+    }
+
+
 
     class CacheData {
         int key, value, frequency;
