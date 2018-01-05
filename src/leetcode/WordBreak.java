80f4cdf3f750a0f3572c3600e101219b5ca402a8
Zheng Lu <zheng.lu@oracle.com>
2018-01-04 17:07:47 -0800

    finished word break

---
+++ src/leetcode/WordBreak.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+ * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * You may assume the dictionary does not contain duplicate words.
+ *
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ *
+ * Return true because "leetcode" can be segmented as "leet code".
+ */
+public class WordBreak {
+
+    // dfs solution
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        if (s.isEmpty() || wordDict.contains(s))
+            return true;
+
+        for (int i = 1; i < s.length(); i++) {
+            String substr = s.substring(0, i);
+
+            if (wordDict.contains(substr) && wordBreak(s.substring(i), wordDict))
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean wordBreakDP(String s, List<String> wordDict){
+
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++){
+            for (int j = i - 1; j >= 0; j--){
+                if (dp[j] && wordDict.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
