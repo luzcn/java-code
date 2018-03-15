2ee880ce1e0c2027f09c03d368583ef2835acb2a
Zheng Lu <zheng.lu@oracle.com>
2018-03-15 10:48:38 -0700

    finshed course schedul 1 and 2

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,14 @@
-import leetcode.GraphValidTree;
+import leetcode.CourseSchedule2;
 
 public class Program {
 
     public static void main(String[] args) {
         // -1, 0, 1, 2, -1, -4
 
-        GraphValidTree ms = new GraphValidTree();
-        int[][] edges = new int[][]{{2, 3}, {1, 2}, {1, 3}};
+        CourseSchedule2 cs = new CourseSchedule2();
+        int[][] edges = new int[][]{{0, 1}, {0, 2}, {1, 3}, {2, 3}, {4, 5}};
 
-        System.out.println(ms.validTree(4, edges));
+        int[] res = cs.findOrder(8, edges);
+
     }
 }
