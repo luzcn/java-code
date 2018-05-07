2a2ce78caf6a7b9ac6774c2de68cc56d48904bcc
Zheng Lu <zheng.lu@oracle.com>
2018-05-07 14:02:24 -0700

    need to finshd kth smallest in sorted matrix

---
+++ src/leetcode/HIndex.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Given an array of citations (each citation is a non-negative integer) of a researcher,
+ * write a function to compute the researcher's h-index.
+ *
+ * According to the definition of h-index on Wikipedia:
+ * "A scientist has index h if h of his/her N papers have at least h citations each,
+ * and the other N − h papers have no more than h citations each."
+ *
+ * For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total
+ * and each of them had received 3, 0, 6, 1, 5 citations respectively.
+ *
+ * Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each,
+ * his h-index is 3.
+ *
+ * Note: If there are several possible values for h, the maximum one is taken as the h-index.
+ */
+public class HIndex {
+
+    // From wiki, we can find the solution:
+    // - sort the citations in descending order.
+    // - for each i, we have "f(i) = min(citations[i], i + 1)"
+    // - the max of "f(i)" is the H-Index.
+    public int hIndex(int[] citations) {
+
+        // sort the citations in descending order
+        int[] sortedCitations = Arrays.stream(citations).boxed().sorted(Comparator.reverseOrder()).mapToInt(x -> x)
+                .toArray();
+
+        int h = 0;
+
+        for (int i = 0; i < sortedCitations.length; i++) {
+            h = Math.max(h, Math.min(sortedCitations[i], i + 1));
+        }
+
+        return h;
+    }
+
+    /**
+     * The input citations array is sorted in ascending order, can we find the h-index in O(logn)
+     */
+    public int hInedex2(int[] citations) {
+
+        // use binary search
+        int n = citations.length;
+        int l = 0, r = n - 1;
+
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+
+            if (citations[mid] == n - mid) {
+                return citations[mid];
+            } else if (citations[mid] < n - mid) {
+                // in ascending order, so we need to check the second half array
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        if (citations[l] >= n - l) {
+            return n - l;
+        }
+
+        if (citations[r] >= n - r) {
+            return n - r;
+        }
+
+        return 0;
+    }
+}
---
+++ src/leetcode/KthSmallestInSortedMatrix.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+// find the kth smallest element in the matrix.
+//
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+//
+// Example:
+//
+// matrix = [
+//    [ 1,  5,  9],
+//    [10, 11, 13],
+//    [12, 13, 15]
+// ],
+// k = 8,
+//
+// return 13.
+public class KthSmallestInSortedMatrix {
+
+
+    private int countEqaulOrLess(int[][] matrix, int target) {
+        int x = matrix.length - 1;
+        int y = 0;
+        int count = 0;
+
+        while (x >= 0 && y < matrix[0].length) {
+            int value = matrix[x][y];
+
+            if (value <= target) {
+                // need to move y to right index
+                y++;
+
+                // all x + 1 elements are less than targets
+                count += x + 1;
+            } else {
+                x--;
+            }
+        }
+
+        return count;
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+
+        // binary search
+
+        if (matrix.length == 0 || k <= 0) {
+            return 0;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        if (k == 1) {
+            return matrix[0][0];
+        }
+
+        int min = matrix[0][0];
+        int max = matrix[m - 1][n - 1];
+
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+
+            int count = countEqaulOrLess(matrix, mid);
+
+            // don't return any numbers yet, if we found k == count
+            // we need to set max = mid - 1 and keep search
+            // until min == max, thus we can guarantee the value "mid" is in matrix
+
+            if (count < k) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+
+        return min;
+    }
+
+
+    // max heap
+    public int kthSmallestHeap(int[][] matrix, int k) {
+        if (matrix.length == 0 || k <= 0) {
+            return 0;
+        }
+
+        // the heap stores an array with 3 elements: value, x and y index
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(Comparator.comparingInt(x -> x[0]));
+
+
+        return 0;
+    }
+}
