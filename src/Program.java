3e1835dbb67fc91f450ec6330531eb4bcec94c40
Zheng Lu <zheng.lu@nordstrom.com>
2018-11-27 16:53:03 -0800

    need to finish 897

--- src/Program.java
+++ src/Program.java
@@ -5,9 +5,6 @@ public class Program {
 
   public static void main(String[] args) {
 
-    OutOfBoundaryPath_576 sol = new OutOfBoundaryPath_576();
-
-    System.out.println(sol.findPaths(1,3, 3, 0, 1));
   }
 
 
