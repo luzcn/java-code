1d63527468b56cb66191d36d8c7b46adc2a3093a
Zheng Lu <zheng.lu@nordstrom.com>
2019-02-27 17:33:39 -0800

    2/27 update

--- src/leetcode/KthLargestElementInArray.java
+++ src/leetcode/KthLargestElementInArray.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.PriorityQueue;
+import java.util.Random;
+
 /**
  * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the
  * sorted order, not the kth distinct element.
@@ -16,6 +19,28 @@ public class KthLargestElementInArray {
     return this.selection(nums, k, 0, nums.length - 1);
   }
 
+  // O(N*logK) time
+  private int findKthLargestPQ(int[] nums, int k) {
+    // find Kth largest, use min heap
+
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+    for (int n : nums) {
+      heap.offer(n);
+
+      if (heap.size() > k) {
+        heap.poll();
+      }
+
+      // if (heap.size() < k) {
+      //   heap.offer(n);
+      // } else {
+      //   heap.poll();
+      //   heap.offer(n);
+      // }
+    }
+
+    return heap.peek();
+  }
 
   private void swap(int[] nums, int i, int j) {
     if (i == j) {
@@ -27,6 +52,7 @@ public class KthLargestElementInArray {
     nums[j] = temp;
   }
 
+  // selection is O(n) average, but O(n^2) worst case
   private int selection(int[] nums, int k, int l, int r) {
     if (l == r) {
       return nums[l];
@@ -53,6 +79,56 @@ public class KthLargestElementInArray {
       return selection(nums, k, l, m - 1);
     } else {
       return selection(nums, k, m + 1, r);
+    }
+  }
+
+  private int selectionIterative(int[] nums, int k) {
+
+    shuffle(nums);
+
+    int l = 0;
+    int r = nums.length - 1;
+
+    while (l <= r) {
+      int m = partition(nums, l, r);
+
+      if (m + 1 == k) {
+        return nums[m];
+      } else if (m + 1 < k) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  private int partition(int[] nums, int l, int r) {
+    int m = l - 1;
+    int pivot = nums[r];
+
+    for (int i = l; i < r; i++) {
+      if (nums[i] > pivot) {
+        m++;
+        swap(nums, i, m);
+      }
+    }
+
+    m++;
+    swap(nums, m, r);
+    return m;
+  }
+
+
+  // randomize the input array can guarantee the selection is O(n)
+  private void shuffle(int[] nums) {
+    Random random = new Random();
+
+    for (int i = nums.length - 1; i > 0; i--) {
+      int j = random.nextInt(i + 1);
+
+      swap(nums, i, j);
     }
   }
 }
--- src/leetcode/MaximumSubarray_53.java
+++ src/leetcode/MaximumSubarray_53.java
@@ -1,14 +1,12 @@
 package leetcode;
 
-/**
- * Given an integer array nums, find the contiguous subarray (containing at least one number) which
- * has the largest sum and return its sum.
- *
- * Example:
- *
- * Input: [-2,1,-3,4,-1,2,1,-5,4], Output: 6 Explanation: [4,-1,2,1] has the largest sum = 6.
- */
-public class MaximumSubarray {
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which
+// has the largest sum and return its sum.
+//
+// Example:
+//
+// Input: [-2,1,-3,4,-1,2,1,-5,4], Output: 6 Explanation: [4,-1,2,1] has the largest sum = 6.
+public class MaximumSubarray_53 {
 
   public int maxSubArray(int[] nums) {
     if (nums.length == 0) {
